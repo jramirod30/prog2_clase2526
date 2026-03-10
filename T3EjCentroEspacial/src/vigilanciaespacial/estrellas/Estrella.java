ee51ed1197b1f9c23f5393683ea4c9106d1ba3af
Maximilian Menschel <maxmenschel5@gmail.com>
2026-03-10 09:58:06 +0100

    fix class estrella and rename file Enunciado

--- T3EjCentroEspacial/src/vigilanciaespacial/estrellas/Estrella.java
+++ T3EjCentroEspacial/src/vigilanciaespacial/estrellas/Estrella.java
@@ -1,32 +1,34 @@
 package vigilanciaespacial.estrellas;
 
 public class Estrella {
-
 	private String identificador;
 	private double campoMagnetico;
 	private int indiceColor;
 	
 	public Estrella(String identificador, double campoMagnetico, int indiceColor) {
-		this.identificador = identificador;
 		this.campoMagnetico = campoMagnetico;
+		this.identificador = identificador;
 		this.indiceColor = indiceColor;
 	}
 	
 	public String getIdentificador() {
-		return "wdaw";
+		return identificador;
 	}
+	
 	public double getCampoMagnetico() {
 		return campoMagnetico;
 	}
+	
 	public int getIndiceColor() {
 		return indiceColor;
 	}
+	
 	public void desplazar() {
 		campoMagnetico -= 5.5;
 		indiceColor -= 1;
 	}
-	public boolean esIgual(Estrella obj) {
-		return this.identificador == obj.identificador;
-	}
 	
+	public boolean esIgual(Estrella obj) {
+		return this.identificador.equals(obj.identificador); 
+	}
 }
